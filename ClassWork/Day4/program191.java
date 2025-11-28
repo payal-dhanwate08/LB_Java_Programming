344bf8c7054ee89c36be98e099952134ca80cf79
Payal Dhanwate <dhanwatepayal@gmail.com>
2025-11-28 23:42:38 +0530

    Add files via upload

---
+++ ClassWork/Day4/program191.java
@@ -0,0 +1,84 @@
+/*
+   iRow : 4
+   iCol : 4
+   
+*       #       #       #
+*       *       #       #
+*       *       *       #
+*       *       *       *
+
+Diagonal pattern
+*/
+import java.util.Scanner;
+
+class Pattern
+{
+    // Filter for squer Matrix
+    public void Display(int iRow,int iCol)
+    {
+        int i = 0,j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("invlid input");
+            System.out.println("row number and column nunber should be same");
+            return;
+        }
+        
+        for(i = 1; i<= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i == j  )
+                {
+                    System.out.printf("%\t");  // Exception
+                }
+                else if(i>j)
+                {
+                    System.out.printf("*\t");
+                }
+                else
+                {
+                     System.out.printf("#\t");
+                }
+                
+            }
+            System.out.println();
+        }
+    }  
+}
+
+
+class program191
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0,iValue2 = 0;
+
+        System.out.println("Enter the number of Rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column :");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
