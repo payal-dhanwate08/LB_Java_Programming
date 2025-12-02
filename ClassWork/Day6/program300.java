e83213ae41c2aca0dd7e887f73d6ead5604fb3d5
Payal Dhanwate <dhanwatepayal@gmail.com>
2025-12-02 23:11:57 +0530

    Add files via upload

---
+++ ClassWork/Day6/program300.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program300
+{
+    public static void main(String A[])
+    {
+       Hashtable <String,Integer> hobj = new Hashtable <String,Integer>();
+
+       hobj.put("PPA",27000);
+       hobj.put("LB",28000);
+       hobj.put("Lsp",29000);
+       hobj.put("Python",30000);
+
+       System.out.println(hobj);
+
+       System.out.println(hobj.get("LB"));
+
+
+    }
+}
